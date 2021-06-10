e6c2e113b45cf2c62ec120bfc51d440face79252
Edison Cai <edisoncai2001@gmail.com>
2021-06-10 14:44:24 +0100

    starting on the warp function to warp perspective

--- document_scanner_project/main.py
+++ document_scanner_project/main.py
@@ -1,8 +1,8 @@
 import cv2.cv2 as cv2
 import numpy as np
 
-widthImg = 640
-heightImg = 480
+widthImg = 480
+heightImg = 640
 
 # Displaying a webcam
 cap = cv2.VideoCapture(0)
@@ -29,7 +29,7 @@ def getContours(img):
         # Make sure the areas are above a certain value to reduce noise
         if area > 5000:
             # -1 for contour index means that we want to draw EVERY contour
-            cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 3)
+            # cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 3)
 
             # Getting the perimeters of every contour, passing in True because they're all closed
             perimeter = cv2.arcLength(cnt, True)
@@ -42,6 +42,8 @@ def getContours(img):
                 biggest = approx
                 maxArea = area
 
+    cv2.drawContours(imgContour, biggest, -1, (255, 0, 0), 20)
+    # Return a list of points for the biggest contour
     return biggest
 
 
@@ -57,21 +59,52 @@ def preProcessing(img):
     return imgThreshold
 
 
+# Function for reordering points so that the getWarp function works properly
+def reorder(myPoints):
+    myPoints = myPoints.reshape((4, 2))
+    myPointsNew = np.zeros((4, 1, 2), np.int32)
+
+    # Get the sum of x's and y's for every point
+    add = myPoints.sum(1)
+
+    # First point is the smallest value in add
+    myPointsNew[0] = myPoints[np.argmin(add)]
+    myPointsNew[3] = myPoints[np.argmax(add)]
+
+    diff = np.diff(myPoints, axis=1)
+    myPointsNew[1] = myPoints[np.argmin(diff)]
+    myPointsNew[2] = myPoints[np.argmax(diff)]
+
+    return myPointsNew
+
+# Function for getting warp properties
 def getWarp(img, biggest):
-    pass
+    biggest = reorder(biggest)
+
+    # Get points of the card and determine which corner they are
+    pts1 = np.float32(biggest)
+    pts2 = np.float32([[0, 0], [widthImg, 0], [0, heightImg], [widthImg, heightImg]])
+    matrix = cv2.getPerspectiveTransform(pts1, pts2)
+    imgOutput = cv2.warpPerspective(img, matrix, (widthImg, heightImg))
+
+    # Cropping the output image
+    imgCropped = imgOutput[20:imgOutput.shape[0] - 20, 20:imgOutput.shape[1] - 20]
+
+    return imgOutput
+
 
 # Infinite loop for displaying frames captured by webcam
 while True:
     success, img = cap.read()
-
     img = cv2.resize(img, (widthImg, heightImg))
     imgContour = img.copy()
 
-    imgThreshold = preProcessing(img)
-    biggest = getContours(imgThreshold)
-    getWarp(img, biggest)
+    imgThres = preProcessing(img)
+    biggest = getContours(imgThres)
+    print(biggest)
+    imgWarped = getWarp(img, biggest)
     # Rotating the webcam for my particular device
-    cv2.imshow("Video", cv2.rotate(imgContour, cv2.ROTATE_90_CLOCKWISE))
+    cv2.imshow("Video", cv2.rotate(imgWarped, cv2.ROTATE_90_CLOCKWISE))
     # If Q is pressed, quit the application
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
